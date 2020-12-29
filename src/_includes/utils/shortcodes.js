70db96fd295649be33a816409271acacc502648f
Alistair Shepherd <alistair@accudio.com>
2020-12-29 19:18:04 +0000

    Removed plaiceholder

--- src/_includes/utils/shortcodes.js
+++ src/_includes/utils/shortcodes.js
@@ -1,9 +1,7 @@
 const icons = require('./shortcodes/icons')
 const divider = require('./shortcodes/divider')
-const plaiceholder = require('./shortcodes/plaiceholder')
 
 module.exports = function(config) {
   config.addShortcode('icon', icons)
   config.addNunjucksAsyncShortcode('divider', divider)
-  config.addNunjucksAsyncShortcode('plaiceholder', plaiceholder)
 }
