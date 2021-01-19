22cc4c3e51751b595bd2e64c49567f6df407df21
Alistair Shepherd <alistair@accudio.com>
2021-01-19 20:24:42 +0000

    Accessibility fixes

--- src/_includes/utils/shortcodes/icons.js
+++ src/_includes/utils/shortcodes/icons.js
@@ -3,7 +3,7 @@ module.exports = function icon(icon, className = '', width = 16, height = 16) {
   const classes = className ? ` ${className}` : ''
 
   return `
-    <span class="b-icon${classes}" role="img" alt="">
+    <span class="b-icon${classes}" aria-hidden="true">
       <svg class="b-icon__svg" width="${width}" height="${height}">
         <use xlink:href="#${icon}" />
       </svg>
