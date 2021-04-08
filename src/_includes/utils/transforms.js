46c29eb77fe17657fa2dfb17fed14e26c3d6d879
Alistair Shepherd <alistair@accudio.com>
2021-04-08 23:26:07 +0100

    Removed PurgeCSS as it's incompatible with how we are loading critical CSS

--- src/_includes/utils/transforms.js
+++ src/_includes/utils/transforms.js
@@ -1,11 +1,11 @@
 const htmlMin = require('./transforms/htmlmin')
 const contentParser = require('./transforms/contentParser')
-const purgeCss = require('./transforms/purgecss')
+// const purgeCss = require('./transforms/purgecss')
 
 module.exports = function(config) {
-  if (process.env.ELEVENTY_ENV === 'production') {
-    config.addTransform('purgecss', purgeCss)
-  }
+  // if (process.env.ELEVENTY_ENV === 'production') {
+  //   config.addTransform('purgecss', purgeCss)
+  // }
 
   config.addTransform('contentParser', contentParser)
 
