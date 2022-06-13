cc77ffc83b5e1692eca96028ea413a7176802c44
Alistair Shepherd <alistair@serieseight.com>
2022-06-13 21:10:32 +0100

    Tried a new system of parallax to smooth it slightly

--- src/assets/js/main/landscape-parallax.js
+++ src/assets/js/main/landscape-parallax.js
@@ -1,12 +1,38 @@
 const container = document.querySelector('.app')
-const elements = document.querySelectorAll('[data-parallax]')
 
+let currentPos = 0
+let scrollPos = 0
 function animation() {
-  const scrollPos = container.scrollTop + 'px'
-  elements.forEach(el => {
-    el.style.setProperty('--scrollPos', scrollPos)
-  })
+  scrollPos = container.scrollTop
+  movePositions()
+  container.style.setProperty('--scrollPos', currentPos + 'px')
   window.requestAnimationFrame(animation)
+}
+
+// if the current position doesn't match the scroll position move it towards it
+function movePositions() {
+  if (currentPos == scrollPos) return
+
+  // get the difference between currentPos and scrollPos
+  const posDiff = Math.abs(currentPos - scrollPos)
+
+  // use difference to calculate a 'speed', or number of px to move this frame.
+  // this means that the time it takes to sync the scroll is dependent on scroll distance, not time.
+  // we divide this so the further away, the faster it moves
+  const speed = Math.max(1, posDiff / 9)
+
+  // if we're close set it to the exact number rather than tweaking it
+  if (posDiff < speed) {
+    currentPos = scrollPos
+    return
+  }
+
+  if (currentPos > scrollPos) {
+    currentPos = currentPos - speed
+    return
+  }
+
+  currentPos = currentPos + speed
 }
 
 window.requestAnimationFrame(animation)
